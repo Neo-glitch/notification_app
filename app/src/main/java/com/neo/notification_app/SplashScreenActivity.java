8503570181f057a08068df677ed127eea04990a2
Neo-glitch <55864433+Neo-glitch@users.noreply.github.com>
2020-03-30 21:29:10 +0100

    splash screen

---
+++ app/src/main/java/com/neo/notification_app/SplashScreenActivity.java
@@ -0,0 +1,51 @@
+package com.neo.notification_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    Handler mHandler;
+    Runnable mRunnable;
+    ImageView img;
+    Animation topAnim, bottomAnim;
+    TextView welcomeMessgae;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        // the animations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        welcomeMessgae = (TextView)findViewById(R.id.splashScreenText);
+        img=findViewById(R.id.image);
+
+        img.setAnimation(topAnim);
+        welcomeMessgae.setAnimation(bottomAnim);
+
+
+        img.animate().alpha(4000).setDuration(0);
+
+        mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent dsp = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(dsp);
+                finish();
+            }
+        }, 3500);
+    }
+}
